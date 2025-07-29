345b4ab843b2b858514358d673d9b1f71312fc5c
ElChimbo30 <elchimborazo30@gmail.com>
2025-07-29 13:28:27 +0200

    Header commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header id="main__header">
+        <div>
+          <h1 class="main__title" id="main__title">
+            Kindle Express
+          </h1>
+        </div>
+        <p class="header-description" id="header-description">
+          Convert and send your books to Kindle in a single click{" "}
+        </p>
+
+        <div id="archivoDiv">
+          <img src="Images/cloud-computing.png" id="img-archivo"></img>
+          <p id="text-archivo">Upload your files</p>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
