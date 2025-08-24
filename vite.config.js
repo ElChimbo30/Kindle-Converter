8cbd40a90e27cf7605081046ba4b82781dd59001
ElChimbo30 <141521792+ElChimbo30@users.noreply.github.com>
2025-08-24 19:03:32 +0200

    New updates on the frontend

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // ← esto es clave
+    port: 5173,
+    allowedHosts: "all",
+  },
+});
